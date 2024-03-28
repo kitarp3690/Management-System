d3956b7d11b4cecc34fc8737cfd949910ad7dc95
GOD <pratikshrestha362@gmail.com>
2024-03-29 00:00:29 +0545

    Included signin also in the same signup file and renamed filename from signup.py to sign.py

--- codes/sign.py
+++ codes/sign.py
@@ -1,12 +1,15 @@
-import streamlit as st
-import psycopg2
-import hashlib
+import streamlit as st  #for UI
+import psycopg2 #for postgres db connection
+import hashlib  #for hashing password 
+import time #for sleep function
 
-class Signup:
+class SignUI:
+    """ This class is used for Signup and Signin operation """
     def __init__(self):
         pass
     
     def start_connection(self):
+        """ This method is used to connect with Database"""
         try:
             self.connection = psycopg2.connect(
                 dbname="streamlit_project",
@@ -21,10 +24,12 @@ class Signup:
             return None
     
     def hash_password(self, password):
+        """ This method is used to generate hashed password"""
         hashed_password=hashlib.sha256(password.encode()).hexdigest()
         return hashed_password
     
     def check_user_existence(self,usname):
+        """ This method is used to check whether username exist or not since username is the primary key in Database"""
         cursor=self.start_connection()
         if cursor:
             try:
@@ -42,6 +47,7 @@ class Signup:
                 cursor.close()
 
     def signup_page(self):
+        """ This method is used for Signup purpose"""
         st.subheader("Sign-Up ")
         username=st.text_input("Username")
         password=st.text_input("Password",type='password')
@@ -64,6 +70,39 @@ class Signup:
                         cursor.execute(command,(username,password_hashed))
                         self.connection.commit()
                         st.success("Signup successful! ")
+                        time.sleep(1.5)
+                    except psycopg2.Error as e:
+                        st.error(f"Error executing sql commands: {e}")
+                    finally:
+                        cursor.close()
+
+    def signin_page(self):
+        """ This method is used for Signin purpose """
+        st.subheader("Signin ")
+        username=st.text_input("Username")
+        password=st.text_input("Password",type='password')
+        if st.button("Login"):
+            if username == "" or password == "":
+                st.error("Fields can't be empty")
+            else:
+                password_hashed=self.hash_password(password)
+                cursor=self.start_connection()
+                if cursor:
+                    try:
+                        command1="select * from users where username=%s"
+                        cursor.execute(command1,(username,))
+                        exist1=cursor.fetchone()
+                        if exist1:
+                            command2="select * from users where username=%s and password=%s"
+                            cursor.execute(command2,(username,password_hashed))
+                            exist2=cursor.fetchone()
+                            if exist2:
+                                time.sleep(1.5)
+                                st.success("Login successful! ")
+                            else:
+                                st.error("Password didn't matched")
+                        else:
+                            st.error("Username doesn't exist")
                     except psycopg2.Error as e:
                         st.error(f"Error executing sql commands: {e}")
                     finally:
@@ -71,4 +110,4 @@ class Signup:
    
 if __name__=="__main__":
     s1 = Signup()
-    s1.signup_page()
+    s1.signin_page()
